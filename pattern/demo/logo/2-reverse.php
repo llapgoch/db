83c7feffd367e888e28ee3ca1f9938c7aaff9ced
Gareth Joyce <gjoyce@iweb.co.uk>
2017-01-09 13:38:52 +0000

    Logo is in looking lovely skunky.

--- pattern/demo/logo/2-reverse.php
+++ pattern/demo/logo/2-reverse.php
@@ -1,9 +1,9 @@
 <?php /*
 Title: Logo
-Class: .logo
+Section: u-fill-neutral-black
 */ ?>
 
-<a href="<?php echo getUrl(); ?>" class="logo" title="Dave Baker">
+<a href="<?php echo getUrl(); ?>" class="logo logo--reverse" title="Dave Baker">
     <span class="logo__letter">d</span>
     <span class="logo__letter logo__b">b</span>
     <span class="u-visually-hidden">Dave Baker</span>
